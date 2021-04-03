6d6ce52032ba884cd04e8b55a229692d900a405e
pesquaiella <pesquaiella@bionexo.com>
2021-04-03 20:51:10 -0300

    incluir carga de dados para cenários de criar e alterar usuário

--- features/support/payload.rb
+++ features/support/payload.rb
@@ -8,7 +8,7 @@ class PayloadUser
         }
     end
 
-    def create_invalid_user
+    def create_user_invalid_email
         {
             name: Faker::Name.name,
             email: 'naoeumemail',
@@ -17,9 +17,51 @@ class PayloadUser
         }
     end
 
+    def create_user_invalid_gender
+        {
+            name: Faker::Name.name,
+            email: Faker::Internet.email,
+            gender: 'INVALID',
+            status: 'Active'
+        }
+    end
+
+    def create_user_invalid_status
+        {
+            name: Faker::Name.name,
+            email: Faker::Internet.email,
+            gender: Faker::Gender.binary_type,
+            status: 'INVALID'
+        }
+    end
+
     def change_name_user
         {
             name: Faker::Name.name
+        }
+    end
+
+    def change_email_user
+        {
+            email: Faker::Internet.email
+        }
+    end
+
+    def gender_male
+        {
+            gender: 'Male'
+        }
+    end
+
+    def gender_female
+        {
+            gender: 'Female'
+        }
+    end
+
+    def change_status_user
+        {
+            status: 'Inactive'
         }
     end
 end
